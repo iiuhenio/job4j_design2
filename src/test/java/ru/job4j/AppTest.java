ff100aed0565848749bb83451a41a5fe04ce2634
iiuhenio <iuheniomail@yandex.ru>
2022-07-07 16:28:28 +0300

    2. Что такое Socket? [#4850]

--- src/test/java/ru/job4j/AppTest.java
+++ src/test/java/ru/job4j/AppTest.java
@@ -14,20 +14,20 @@ public class AppTest extends TestCase {
      * @param testName name of the test case
      */
     public AppTest(String testName) {
-        super( testName );
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
     public static Test suite() {
-        return new TestSuite( AppTest.class );
+        return new TestSuite(AppTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
     public void testApp() {
-        assertTrue( true );
+        assertTrue(true);
     }
 }
