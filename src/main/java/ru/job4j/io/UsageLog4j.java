d162f0c2b6613602a4474c060880c3544f8c864a
iiuhenio <iuheniomail@yandex.ru>
2022-07-12 12:41:38 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,17 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Petr Arsentev";
+        int age = 33;
+        Boolean male = true;
+        char female = 'n';
+        long life = 99999999;
+        double temp = 36.6;
+        byte adulthood = 18;
+        short birthday = 6;
+        float start = 0;
+        LOG.debug("User info name : {}, age : {}, male : {}, female : {}, life : {}, "
+                 + "temp : {}, adulthood : {}, birthday : {}, start : {}", name, age,
+                 male, female, life, temp, adulthood, birthday, start);
     }
 }
