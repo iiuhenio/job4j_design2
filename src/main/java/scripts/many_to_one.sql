fabc80a894c3dc08abea02c3e701b060f5dd4fc6
iiuhenio <iuheniomail@yandex.ru>
2022-08-18 17:06:30 +0300

    0.1. Связи между таблицами [#373615]

--- src/main/java/scripts/many_to_one.sql
+++ src/main/java/scripts/many_to_one.sql
@@ -1,17 +1,17 @@
-create table orders(
+create table orderer(
     id serial primary key,
     name varchar(255)
 );
 
-create table orderer(
+create table orders(
     id serial primary key,
     name varchar(255),
-    position_id int references orders(id)
+    position_id int references orderer(id)
 );
 
-insert into orders(name) values ('12345');
-insert into orderer(name, position_id) VALUES ('Eugene', 1);
+insert into orderer(name) values ('Eugene');
+insert into orders(name, position_id) VALUES ('1125', 1);
 
-select * from orderer;
+select * from orders;
 
-select * from orders where id in (select id from orderer);
+select * from orderer where id in (select id from orders);
--- src/test/java/ru/job4j/io/ArgsNameTest.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.io;
-
-import junit.framework.TestCase;
-
-public class ArgsNameTest extends TestCase {
-
-}
