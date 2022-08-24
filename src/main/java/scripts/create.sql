8ef00c2d145b42523095dbecd6adb9fc3d7c5040
iiuhenio <iuheniomail@yandex.ru>
2022-08-24 17:25:27 +0300

    1. Схема прав пользователи и роли. [#1093]

---
+++ src/main/java/scripts/create.sql
@@ -0,0 +1,52 @@
+create table role (
+    id serial primary key,
+    name text
+);
+
+create table users (
+    id serial primary key,
+    name text,
+    role_id int references role(id)
+);
+
+create table rules (
+    id serial primary key,
+    name text
+)
+
+create table role_rules (
+    id serial primary key,
+    name text,
+    rules_id int references rules(id),
+    role_id int references role(id)
+);
+
+create table item;
+    id serial primary key,
+    name text,
+    users_id int references users(id),
+    state_id int references state(id),
+    category_id int references category(id)
+);
+
+create table comments (
+    id serial primary key,
+    name text,
+    item_id int references item(id)
+);
+
+create table attachs(
+    id serial primary key,
+    name text,
+    item_id int references item(id)
+);
+
+create table category(
+    id serial primary key,
+    name text
+);
+
+create table state (
+    id serial primary key,
+    name text
+);
