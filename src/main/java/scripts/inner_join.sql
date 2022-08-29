ca9c9796ab91869dc13612497f47496f2f35e196
iiuhenio <iuheniomail@yandex.ru>
2022-08-29 15:08:21 +0300

    1. Inner join [#373617]

---
+++ src/main/java/scripts/inner_join.sql
@@ -0,0 +1,30 @@
+CREATE table presient (
+    id serial primary key,
+    name text,
+    surname text
+);
+
+create table country(
+    id serial primary key,
+    name varchar(255),
+    president_id int references president(id) unique
+);
+
+insert into president(name, surname) values ('Joe', 'Biden');
+insert into president(name, surname) values ('Vladimir', 'Putin');
+insert into president(name, surname) values ('Angela', 'Merkel');
+
+insert into country(name, president_id) values ('USA', 1);
+insert into country(name, president_id) values ('Russia', 2);
+insert into country(name, president_id) values ('Germany', 3);
+insert into country(name) values ('Italy');
+insert into country(name) values ('France');
+
+select pp.name, p.name, p.surname
+from country as pp join president as p on pp.president_id = p.id;
+
+select pp.name as Название, p.name as Имя, p.surname as Фамилия
+from country as pp join president as p on pp.president_id = p.id;
+
+select pp.name as "Название страны", p.name, p.surname Фамилия
+from country pp join president p on pp.president_id = p.id;
