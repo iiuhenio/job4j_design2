e8fd07c7e3989f0a1d2f015e6001e8955beeea4a
iiuhenio <iuheniomail@yandex.ru>
2023-02-15 14:42:25 +0300

    7. Анализ доступности сервера [#859 #308330]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -12,13 +12,13 @@ import java.util.List;
  * Сервер не работал, если status = 400 или 500.
  * Диапазоном считается последовательность статусов 400 и 500
  */
-public class Analizy {
+public class Analysis {
     public void unavailable(String source, String target) {
         boolean flag = true;
         String start = null;
         String end;
-        List<String> status = new ArrayList<>();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source));
+             PrintWriter write = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
             for (String line = bufferedReader.readLine(); line != null; line = bufferedReader.readLine()) {
                 String[] out = line.split(" ");
                 if (flag && line.startsWith("400") || flag && line.startsWith("500")) {
@@ -27,23 +27,16 @@ public class Analizy {
                 } else if (!flag && line.startsWith("200") || line.startsWith("300")) {
                     flag = true;
                     end = out[1];
-                    status.add(start + "; " + end);
+                    write.println(start + "; " + end);
                 }
-            }
-            try (PrintWriter write = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
-                for (String s : status) {
-                    write.println(s);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace(System.out);
         }
     }
 
     public static void main(String[] args) {
-        Analizy analizy = new Analizy();
-        analizy.unavailable("source.csv", "target.csv");
+        Analysis analysis = new Analysis();
+        analysis.unavailable("data/server.log", "data/target.csv");
     }
 }
