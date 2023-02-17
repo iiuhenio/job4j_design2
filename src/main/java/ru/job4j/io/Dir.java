03361d18e1a94640ac40734be2ddfdfbdcad0adf
iiuhenio <iuheniomail@yandex.ru>
2023-02-17 17:54:41 +0300

    12. Валидация параметров запуска [#246865 #392596]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -3,10 +3,10 @@ package ru.job4j.io;
 import java.io.File;
 
 public class Dir {
+
+    public String[] args;
+
     public static void main(String[] args) {
-        if (args.length == 0) {
-            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
-        }
         File file = new File(args[0]);
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
@@ -14,9 +14,24 @@ public class Dir {
         if (!file.isDirectory()) {
             throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
-        System.out.println(String.format("size : %s", file.getTotalSpace()));
-        for (File subfile : file.listFiles()) {
-            System.out.println(subfile.getName());
+        if (validation(args)) {
+            System.out.println(String.format("size : %s", file.getTotalSpace()));
+            for (File subfile : file.listFiles()) {
+                if (subfile.getName().endsWith(args[1])) {
+                    System.out.println(subfile.getName());
+                }
+            }
         }
+    }
+
+    public static boolean validation(String[] args) {
+        boolean rsl = true;
+        File file = new File(args[0]);
+        if (!file.isDirectory() || !file.exists()
+                || !args[1].startsWith(".") || args[1].length() < 2
+                || args.length != 2) {
+            rsl = false;
+        }
+        return rsl;
     }
 }
