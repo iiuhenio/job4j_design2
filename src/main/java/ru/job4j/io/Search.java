6747bfbfbb45023cc8ff51826a47042825a90523
iiuhenio <iuheniomail@yandex.ru>
2023-02-20 13:18:02 +0300

    12. Валидация параметров запуска [#246865 #392596]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,13 +14,26 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".txt")).forEach(System.out::println);
+        Path file = Paths.get(args[0]);
+        if (validation(args)) {
+            search(file, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static boolean validation(String[] args) {
+        boolean rsl = true;
+        File file = new File(args[0]);
+        if (!file.isDirectory() || !file.exists()
+                || !args[1].startsWith(".") || args[1].length() < 2
+                || args.length != 2) {
+            rsl = false;
+        }
+        return rsl;
     }
 }
