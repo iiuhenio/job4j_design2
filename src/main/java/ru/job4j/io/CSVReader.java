f1e45afe1aecb5a0ba398da36bdfae9e13335830
iiuhenio <iuheniomail@yandex.ru>
2023-03-09 15:56:47 +0300

    17. Scanner [#504791 #397914]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -2,6 +2,7 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.StringJoiner;
 
@@ -10,20 +11,27 @@ public class CSVReader {
      * Данный метод должен принимать файл, обрабатывать его и выводить содержимое на экран
      */
     public static void handle(ArgsName argsName) throws Exception {
-        try (FileReader fileReader = new FileReader("info.txt");
+        File source = new File(new File(argsName.get("path")).getAbsolutePath());
+        File target = new File(new File(argsName.get("out")).getAbsolutePath());
+        try (FileReader fileReader = new FileReader(source);
+             var scan = new Scanner(fileReader).useDelimiter(",");
              Scanner scanner = new Scanner(fileReader)) {
-            while (scanner.hasNext()) {
-                System.out.print(scanner.nextInt());
-                System.out.println(scanner.next());
+            while (scan.hasNext()) {
+                if (Objects.equals(argsName.get("out"), "stdout")) {
+                    System.out.print(scan.next());
+                }
+                if (!Objects.equals(argsName.get("out"), "stdout")) {
+                    try (BufferedOutputStream out =
+                                 new BufferedOutputStream(new FileOutputStream(target))) {
+                        out.write(scanner.next().getBytes());
+                    }
+                }
             }
         }
     }
 
     public static void main(String[] args) throws Exception {
-        ArgsName an = new ArgsName();
-        System.out.println(an.get("path"));
-
-        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
-        System.out.println(jvm.get("Xmx"));
+        ArgsName an = ArgsName.of(args);
+        CSVReader.handle(an);
     }
 }
