5c04455492699e5fa8fcc326715960d2956f2ff8
iiuhenio <iuheniomail@yandex.ru>
2023-03-16 16:47:31 +0300

    17. Scanner [#504791 #397914]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -11,19 +11,35 @@ public class CSVReader {
      */
     public static void handle(ArgsName argsName) throws Exception {
 
+        /*
+         *  Ниже объявлены:
+         * filter[] - по каким словам фильтруем
+         * lines - поледние строчки файла
+         * файлы откуда читаем и куда записываем
+         */
         String[] filter = argsName.get("filter").split(",");
         List<String> lines = new ArrayList<>();
         File source = Path.of(argsName.get("path")).toFile();
         File target = Path.of(argsName.get("out")).toFile();
 
+        List<String> lastLines = new ArrayList<>();
+
+        System.out.println(Arrays.toString(filter));
+
+        /*
+         *  names[] - все возможные фильтры
+         */
         try (FileReader fileReader = new FileReader(source);
-            /* var scan = new Scanner(fileReader).useDelimiter(","); */
+            var scan = new Scanner(fileReader).useDelimiter(",");
             Scanner scanner = new Scanner(fileReader).useDelimiter(System.lineSeparator())) {
-            String[] names = scanner.nextLine().replaceAll("\"", "").split(argsName.get("delimiter"));
+            String[] names = scanner.nextLine().split(argsName.get("delimiter"));
             while (scanner.hasNext()) {
                 lines.add(scanner.next());
             }
 
+            /*
+             * position[] - какие столбцы присутствуют в заданном фильтре и их индекс
+             */
             int[] position = new int[names.length];
             Arrays.fill(position, -1);
             for (int i = 0; i < names.length; i++) {
@@ -33,39 +49,45 @@ public class CSVReader {
                     }
                 }
             }
-            System.out.println("filter = " + Arrays.toString(filter));
-            System.out.println("names = " + Arrays.toString(names));
-            System.out.println("lines = " + lines);
-            System.out.println("position = " + Arrays.toString(position));
 
+            /*
+             * lines1[] - вся первая строка
+             * answer[] - отфильтрованные строки (из первой(второй) строки)
+             */
             String[] answer = new String[filter.length];
-            String line = lines.get(0);
-            String[] lines1 = line.replaceAll("\"", "").split(",");
-            for (int i = 0; i < position.length; i++) {
-                for (int j = 0; j < lines1.length; j++) {
+            for (int j = 0; j < lines.size(); j++) {
+                String line = lines.get(j);
+                String[] lines1 = line.split(argsName.get("delimiter"));
+                for (int i = 0; i < position.length; i++) {
                     if (position[i] != -1) {
-                        answer[position[i]] == lines1[j];
+                        answer[position[i]] = lines1[i];
                     }
                 }
-            }
-            System.out.println("lines1 = " + Arrays.toString(lines1));
-            System.out.println("answer = " + Arrays.toString(answer));
-
-        }
+                String answer1 = Arrays.toString(answer);
+                lastLines.add(answer1);
 
                 /*
-                if (Objects.equals(argsName.get("out"), "stdout")) {
-                    System.out.print(scan.next());
-                }
-                if (!Objects.equals(argsName.get("out"), "stdout")) {
-                    try (BufferedOutputStream out =
-                                 new BufferedOutputStream(new FileOutputStream(target))) {
-                        out.write(scan.next().getBytes());
-                    }
+                System.out.println("В массивах сейчас: ");
+                System.out.println("filter = " + Arrays.toString(filter));
+                System.out.println("names = " + Arrays.toString(names));
+                System.out.println("lines = " + lines);
+                System.out.println("position = " + Arrays.toString(position));
+                System.out.println("lines1 = " + Arrays.toString(lines1));
+                System.out.println("answer = " + Arrays.toString(answer));
+                System.out.println("answer1 = " + answer1);
+                */
+            }
+
+            if (Objects.equals(argsName.get("out"), "stdout")) {
+                System.out.println(lastLines);
+            }
+            if (!Objects.equals(argsName.get("out"), "stdout")) {
+                try (BufferedOutputStream out =
+                             new BufferedOutputStream(new FileOutputStream(target))) {
+                    out.write(scan.next().getBytes());
                 }
             }
-                 */
-
+        }
     }
 
     public static void main(String[] args) throws Exception {
