9503ce0648589b965e90a3aec892f8b5c198be05
iiuhenio <iuheniomail@yandex.ru>
2023-04-21 11:34:06 +0300

    Разное

--- src/main/java/scripts/script.sql
+++ src/main/java/scripts/script.sql
@@ -5,6 +5,7 @@ area text,
 length money
 );
 
+
 insert into rivers (name, area, length) values ('Sena', 'France', 18000);
 update rivers set length = '17000';
 insert into rivers (name, area, length) values ('Moscow', 'Russia', 14000);
