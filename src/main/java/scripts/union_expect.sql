07a4a60fa22b3ec3970f7a255ad6ea13d6a984a7
iiuhenio <iuheniomail@yandex.ru>
2023-04-26 13:40:19 +0300

    10. Операторы множества в SQL [#504896]

---
+++ src/main/java/scripts/union_expect.sql
@@ -0,0 +1,59 @@
+CREATE TABLE movie (
+    id SERIAL PRIMARY KEY,
+    name text,
+    director text
+);
+
+CREATE TABLE book (
+    id SERIAL PRIMARY KEY,
+    title text,
+    author text
+);
+
+INSERT INTO movie (name, director)
+VALUES ('Марсианин', 'Ридли Скотт'),
+       ('Матрица', 'Братья Вачовски'),
+       ('Властелин колец', 'Питер Джексон'),
+       ('Гарри Поттер и узник Азкабана', 'Альфонсо Куарон'),
+       ('Железный человек', 'Джон Фавро');
+
+INSERT INTO book (title, author)
+VALUES ('Гарри Поттер и узник Азкабана', 'Джоан Роулинг'),
+       ('Властелин колец', 'Джон Толкин'),
+       ('1984', 'Джордж Оруэлл'),
+       ('Марсианин', 'Энди Уир'),
+       ('Божественная комедия', 'Данте Алигьери');
+
+
+SELECT name
+FROM movie
+INTERSECT
+select title
+from book
+
+(select name
+from movie
+union
+SELECT title
+FROM book)
+except
+select name
+from movie
+
+((SELECT name
+FROM movie
+union
+select title
+from book)
+EXCEPT
+select title
+from book)
+union
+(select name
+from movie
+union
+(SELECT title
+FROM book)
+except
+select name
+from movie)
