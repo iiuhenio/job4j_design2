94e4d0a2d0358b48d83aea899e2070cac132ecec
iiuhenio <iuheniomail@yandex.ru>
2023-05-11 13:24:40 +0300

    0.2. PreparedStatement

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -10,104 +10,68 @@ public class TableEditor implements AutoCloseable {
 
     private Properties properties;
 
-    public TableEditor(Properties properties) {
+    public TableEditor(Properties properties) throws ClassNotFoundException, SQLException {
         this.properties = properties;
         initConnection();
     }
 
-    private void initConnection() {
-        connection = null;
-    }
+    private Connection initConnection() throws ClassNotFoundException, SQLException {
+        properties.setProperty("driver", "org.postgresql.Driver");
+        properties.setProperty("url", "jdbc:postgresql://localhost:5432/idea_db");
+        properties.setProperty("login", "postgres");
+        properties.setProperty("password", "password");
 
-    private static Connection getConnection() throws Exception {
-        Class.forName("org.postgresql.Driver");
-        String url = "jdbc:postgresql://localhost:5432/idea_db";
-        String login = "postgres";
-        String password = "password";
+        Class.forName(properties.getProperty("driver"));
+        String url = properties.getProperty("url");
+        String login = properties.getProperty("login");
+        String password = properties.getProperty("password");
+
         return DriverManager.getConnection(url, login, password);
+
+        /* connection = null; */
+
     }
 
-    public static void main(String[] args) throws Exception {
-        try (Connection connection = getConnection()) {
-            try (Statement statement = connection.createStatement()) {
-                String sql = String.format(
-                        "create table if not exists demo_table(%s, %s);",
-                        "id serial primary key",
-                        "name text"
-                );
-                statement.execute(sql);
-                System.out.println(getTableScheme(connection, "demo_table"));
-            }
-        }
-    }
+    public void createTable(String tableName) throws SQLException, ClassNotFoundException {
+        try (Connection connection = initConnection();) {
+            try {
+                assert connection != null;
+                try (Statement statement = connection.createStatement()) {
+                    String sql = String.format(
+                            "create table if not exists demo_table(%s, %s);",
+                            "id serial primary key",
+                            "name text"
+                    );
+                    statement.execute(sql);
 
-    public void createTable(String tableName) throws Exception {
-            try (Statement statement = connection.createStatement()) {
-                String sql = String.format(
-                        "create table %s();",
-                        tableName
-                );
-                statement.execute(sql);
-                System.out.println(getTableScheme(connection, "tableName"));
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
             }
         }
 
-    public void dropTable(String tableName) throws Exception {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format(
-                    "drop table %s;",
-                    tableName
-            );
-            statement.execute(sql);
-            System.out.println(getTableScheme(connection, "tableName"));
-        }
     }
 
-    public void addColumn(String tableName, String columnName, String type) throws Exception {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format(
-                    "ALTER TABLE %s ADD COLUMN %s type;",
-                    tableName,
-                    columnName
-            );
-            statement.execute(sql);
-            System.out.println(getTableScheme(connection, "tableName"));
-        }
+    public void dropTable(String tableName) {
     }
 
-    public void dropColumn(String tableName, String columnName) throws Exception {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format(
-                    "ALTER TABLE %s DROP COLUMN %s;",
-                    tableName,
-                    columnName
-            );
-            statement.execute(sql);
-            System.out.println(getTableScheme(connection, "tableName"));
-        }
+    public void addColumn(String tableName, String columnName, String type) {
     }
 
-    public void renameColumn(String tableName, String columnName, String newColumnName) throws Exception {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format(
-                    "ALTER TABLE %s RENAME COLUMN %s TO %s;",
-                    tableName,
-                    columnName,
-                    newColumnName
-            );
-            statement.execute(sql);
-            System.out.println(getTableScheme(connection, "tableName"));
-        }
+    public void dropColumn(String tableName, String columnName) {
     }
 
-    public static String getTableScheme(Connection connection, String tableName) throws Exception {
+    public void renameColumn(String tableName, String columnName, String newColumnName) {
+    }
+
+    public String getTableScheme(String tableName) throws Exception {
         var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
         var header = String.format("%-15s|%-15s%n", "NAME", "TYPE");
         var buffer = new StringJoiner(rowSeparator, rowSeparator, rowSeparator);
         buffer.add(header);
         try (var statement = connection.createStatement()) {
             var selection = statement.executeQuery(String.format(
-                    "select * from %s limit 1", tableName
+                    "SELECT * FROM %s LIMIT 1", tableName
             ));
             var metaData = selection.getMetaData();
             for (int i = 1; i <= metaData.getColumnCount(); i++) {
@@ -124,5 +88,13 @@ public class TableEditor implements AutoCloseable {
         if (connection != null) {
             connection.close();
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        TableEditor te = new TableEditor(new Properties());
+
+        te.initConnection();
+        te.createTable("table1");
+        System.out.println(te.getTableScheme("table1"));
     }
 }
